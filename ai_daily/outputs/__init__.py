d9532f28f372c5776c5fbb4eb2b17feb77e56b76
Alexis Balayre <alexis@balayre.com>
2026-02-06 13:49:08 +0100

    enable github trends repo dedicated newsletter
    Signed-off-by: Alexis Balayre <alexis@balayre.com>

--- ai_daily/outputs/__init__.py
+++ ai_daily/outputs/__init__.py
@@ -1,7 +1,8 @@
 """Output generators (newsletter, TTS, etc.)."""
 
+from ai_daily.outputs.github_newsletter import GitHubNewsletterOutput
 from ai_daily.outputs.newsletter import NewsletterOutput
 from ai_daily.outputs.summary_generator import SummaryGenerator
 from ai_daily.outputs.tts_briefing import TTSBriefingOutput
 
-__all__ = ["NewsletterOutput", "SummaryGenerator", "TTSBriefingOutput"]
+__all__ = ["GitHubNewsletterOutput", "NewsletterOutput", "SummaryGenerator", "TTSBriefingOutput"]
